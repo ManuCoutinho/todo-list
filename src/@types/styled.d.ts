782eca7af400d3e1857eb6034d1137b3e784be3f
ManuCoutinho <gdn.sasha@gmail.com>
2022-10-24 15:56:24 -0300

    chore: revert deps

---
+++ src/@types/styled.d.ts
@@ -0,0 +1,24 @@
+import 'styled-components'
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    title: string
+
+    colors: {
+      mainBackground: string
+      backgroundInput: string
+      textMuted: string
+      textPrimary: string
+      textSecondary: string
+      textDefault: string
+      borderColor: string
+      buttonCreateTask: string
+      checkboxTask: string
+      buttonDeleteTask: string
+      primary: string
+      glassColor: string
+      borderGlass: string
+      toggleTheme: string
+    }
+  }
+}
---
+++ src/components/ErrorBoundary/NotFound.tsx
@@ -0,0 +1,7 @@
+import * as Styled from './styles'
+
+const NotFound: React.FC = () => {
+  return <Styled.Container></Styled.Container>
+}
+
+export default NotFound
